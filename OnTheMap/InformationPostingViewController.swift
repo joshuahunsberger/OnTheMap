2a97c4c4c8ff91a7b5af6bbdbcc9d9ea76d40bda
Joshua Hunsberger <joshua@hunsberger.co>
2016-02-17 22:14:43 -0500

    Add InformationPostingViewController.swift

---
+++ OnTheMap/InformationPostingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InformationPostingViewController.swift
+//  OnTheMap
+//
+//  Created by Joshua Hunsberger on 2/17/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class InformationPostingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
