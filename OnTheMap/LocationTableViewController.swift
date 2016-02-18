8902b4adb002d39e85d2b5298a206007a67c1ea8
Joshua Hunsberger <joshua@hunsberger.co>
2016-02-17 22:11:43 -0500

    Add LocationTableViewController.swift

---
+++ OnTheMap/LocationTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LocationTableViewController.swift
+//  OnTheMap
+//
+//  Created by Joshua Hunsberger on 2/17/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
