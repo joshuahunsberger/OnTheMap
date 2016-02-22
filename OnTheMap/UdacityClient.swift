ec901790eaf67920c660e8324dea43378da41f51
Joshua Hunsberger <joshua@hunsberger.co>
2016-02-21 19:51:45 -0500

    Add UdacityClient.swift

---
+++ OnTheMap/UdacityClient.swift
@@ -0,0 +1,17 @@
+//
+//  UdacityClient.swift
+//  OnTheMap
+//
+//  Created by Joshua Hunsberger on 2/21/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import Foundation
+
+class UdacityClient : NSObject {
+    
+    override init() {
+        super.init()
+    }
+    
+}
