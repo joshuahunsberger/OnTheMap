c0f8cead156a603e2ac9f2e78a7032d0ed1038d4
Joshua Hunsberger <joshua@hunsberger.co>
2016-02-22 00:29:36 -0500

    Add UdacityConvenience.swif

---
+++ OnTheMap/UdacityConvenience.swift
@@ -0,0 +1,17 @@
+//
+//  UdacityConvenience.swift
+//  OnTheMap
+//
+//  Created by Joshua Hunsberger on 2/22/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+// MARK: UdacityClient (Convenience Methods)
+extension UdacityClient {
+    
+    // TODO: Implement method to POST username and password to create session
+    
+    // TODO: Implement method to DELETE the current session
+    
+    // TODO: Implement method to GET public user data
+}
