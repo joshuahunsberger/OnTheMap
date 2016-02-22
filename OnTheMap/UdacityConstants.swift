710d5f5b800a59dfa0e612ff382ad063619b742c
Joshua Hunsberger <joshua@hunsberger.co>
2016-02-22 00:01:32 -0500

    Add UdacityConstants.swift as extension to UdacityClient class

---
+++ OnTheMap/UdacityConstants.swift
@@ -0,0 +1,46 @@
+//
+//  UdacityConstants.swift
+//  OnTheMap
+//
+//  Created by Joshua Hunsberger on 2/21/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+// MARK: UdacityClient (Constants)
+
+extension UdacityClient {
+    
+    // MARK: Constants
+    struct Constants {
+        
+        // MARK: URL Components
+        static let ApiScheme = "https"
+        static let ApiHost = "udacity.com"
+        static let ApiPath = "/api"
+    }
+    
+    // MARK: Methods
+    struct Methods {
+        static let session = "/session"
+        static let users = "/users/"
+    }
+    
+    struct JSONBodyKeys {
+        static let dictionaryName = "udacity"
+        static let userName = "username"
+        static let password = "password"
+    }
+    
+    struct JSONResponseKeys {
+        // MARK: Dictionary Names
+        static let account = "account"
+        static let session = "session"
+        static let user = "user"
+        
+        // MARK: Key names
+        static let accountKey = "key"
+        static let sessionID = "id"
+        static let userFirstName = "first_name"
+        static let userLastName = "last_name"
+    }
+}
