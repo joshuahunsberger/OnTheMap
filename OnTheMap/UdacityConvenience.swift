7c41e95f1f75a5876242650ba19669f6c3498d5d
Joshua Hunsberger <joshua@hunsberger.co>
2016-02-24 19:58:57 -0500

    Create postLogin method

--- OnTheMap/UdacityConvenience.swift
+++ OnTheMap/UdacityConvenience.swift
@@ -6,11 +6,36 @@
 //  Copyright © 2016 Joshua Hunsberger. All rights reserved.
 //
 
+import Foundation
+
 // MARK: UdacityClient (Convenience Methods)
 extension UdacityClient {
     
     // TODO: Implement method to POST username and password to create session
-    
+    func postLogin(username: String, password: String, completionHandlerForSession: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        var jsonBody = [String: AnyObject]()
+        jsonBody["\(UdacityClient.JSONBodyKeys.dictionaryName)"] = "{\"username\": \"\(username)\", \"password\": \"\(password)\"}"
+        
+        taskForPostMethod(UdacityClient.Methods.session, parameters: nil, jsonBody: jsonBody) { (results, error) in
+            
+            if let error = error {
+                completionHandlerForSession(result: nil, error: error)
+            } else {
+                guard let session = results[UdacityClient.JSONResponseKeys.session] as? [String: AnyObject] else{
+                    completionHandlerForSession(result: nil, error: NSError(domain: "postSession parsing", code: 1, userInfo: [NSLocalizedDescriptionKey: "Could not parse postLogin dictionary"]))
+                    return
+                }
+                
+                guard let sessionID = session[UdacityClient.JSONResponseKeys.sessionID] as? String else{
+                    completionHandlerForSession(result: nil, error: NSError(domain: "postSession parsing sessionID", code: 1, userInfo: [NSLocalizedDescriptionKey: "Could not parse sessionID"]))
+                    return
+                }
+                
+                completionHandlerForSession(result: sessionID, error: nil)
+            }
+        }
+    }
     // TODO: Implement method to DELETE the current session
     
     // TODO: Implement method to GET public user data
