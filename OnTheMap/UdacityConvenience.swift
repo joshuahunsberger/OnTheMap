107648fc5c9ec2cbbad7a1d87a8699857853b9a0
Joshua Hunsberger <joshua@hunsberger.co>
2016-02-29 23:26:10 -0500

    Add deleteSession method

--- OnTheMap/UdacityConvenience.swift
+++ OnTheMap/UdacityConvenience.swift
@@ -11,7 +11,6 @@ import Foundation
 // MARK: UdacityClient (Convenience Methods)
 extension UdacityClient {
     
-    // TODO: Implement method to POST username and password to create session
     func postLogin(username: String, password: String, completionHandlerForSession: (success: Bool, error: NSError?) -> Void) {
         
         let jsonBody =  "{\"udacity\" : {\"username\": \"\(username)\", \"password\": \"\(password)\"}}"
@@ -21,12 +20,12 @@ extension UdacityClient {
             if let error = error {
                 completionHandlerForSession(success: false, error: error)
             } else {
-                guard let session = results[UdacityClient.JSONResponseKeys.session] as? [String: AnyObject] else{
+                guard let session = results[UdacityClient.JSONResponseKeys.session] as? [String: AnyObject] else {
                     completionHandlerForSession(success: false, error: NSError(domain: "postSession parsing", code: 1, userInfo: [NSLocalizedDescriptionKey: "Could not parse postLogin dictionary"]))
                     return
                 }
                 
-                guard let sessionID = session[UdacityClient.JSONResponseKeys.sessionID] as? String else{
+                guard let sessionID = session[UdacityClient.JSONResponseKeys.sessionID] as? String else {
                     completionHandlerForSession(success: false, error: NSError(domain: "postSession parsing sessionID", code: 1, userInfo: [NSLocalizedDescriptionKey: "Could not parse sessionID"]))
                     return
                 }
@@ -35,7 +34,20 @@ extension UdacityClient {
             }
         }
     }
+    
     // TODO: Implement method to DELETE the current session
+    func deleteSession(completionHandlerForDelete: (success: Bool, error: NSError?) -> Void){
+        let method = UdacityClient.Methods.session
+        
+        taskForDeleteMethod(method) { (results, error) in
+            if let error = error {
+                completionHandlerForDelete(success: false, error: error)
+            } else {
+                print(results)
+                completionHandlerForDelete(success: true, error: nil)
+            }
+        }
+    }
     
     // TODO: Implement method to GET public user data
 }
