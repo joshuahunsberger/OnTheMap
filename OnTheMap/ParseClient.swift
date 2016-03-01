bda6f1d828c74cb95ca8df0ca71294fd1bc4c989
Joshua Hunsberger <joshua@hunsberger.co>
2016-03-01 18:58:02 -0500

    Add ParseClient.swift

---
+++ OnTheMap/ParseClient.swift
@@ -0,0 +1,18 @@
+//
+//  ParseClient.swift
+//  OnTheMap
+//
+//  Created by Joshua Hunsberger on 3/1/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import Foundation
+
+class ParseClient: NSObject {
+    
+    // MARK: Properties
+    
+    // MARK: GET
+    
+    // MARK: PUT
+}
