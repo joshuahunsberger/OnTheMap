39e72d6a62aa9fc5a48c962fa2b995f79213feb3
Joshua Hunsberger <joshua@hunsberger.co>
2016-03-03 21:20:40 -0500

    Add ParseConvenience.swift

---
+++ OnTheMap/ParseConvenience.swift
@@ -0,0 +1,18 @@
+//
+//  ParseConvenience.swift
+//  OnTheMap
+//
+//  Created by Joshua Hunsberger on 3/3/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import Foundation
+
+// MARK: ParseClient (Convenience Methods)
+
+extension ParseClient {
+    
+    //TODO: Create method to retrieve student locations with GET
+    
+    //TODO: Create method to send student location with POST
+}
