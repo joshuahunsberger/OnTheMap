77616bc81d432f7ad337f1dd03e3cafa2dce7c18
Joshua Hunsberger <joshua@hunsberger.co>
2016-03-03 22:10:38 -0500

    Add StudentLocation model class

---
+++ OnTheMap/StudentLocation.swift
@@ -0,0 +1,41 @@
+//
+//  StudentLocation.swift
+//  OnTheMap
+//
+//  Created by Joshua Hunsberger on 3/3/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import Foundation
+
+class StudentLocation : NSObject {
+    
+    //MARK: Properties
+    
+    let objectID: String?
+    let uniqueKey: String?
+    let firstName: String?
+    let lastName: String?
+    let mapString: String?
+    let mediaURL: String?
+    let latitude: Double?
+    let longitude: Double?
+    
+    //MARK: Initializers
+    
+    
+    
+    init(objectID: String, uniqueKey: String, firstName: String, lastName: String, mapString: String, mediaURL: String, latitude: Double, longitude: Double) {
+        self.objectID = objectID
+        self.uniqueKey = uniqueKey
+        self.firstName = firstName
+        self.lastName = lastName
+        self.mapString = mapString
+        self.mediaURL = mediaURL
+        self.latitude = latitude
+        self.longitude = longitude
+        
+        super.init()
+    }
+    
+}
