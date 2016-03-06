2d3f462f2d5df002fdb24dd7fa5f22b4ae92df9b
Joshua Hunsberger <joshua@hunsberger.co>
2016-03-06 18:47:41 -0500

    Add TabBarViewController.swift and set as view controller for tab bar in storyboard

---
+++ OnTheMap/TabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarViewController.swift
+//  OnTheMap
+//
+//  Created by Joshua Hunsberger on 3/6/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    
+    @IBOutlet weak var logoutButton: UIBarButtonItem!
+    @IBOutlet weak var addLocationButton: UIBarButtonItem!
+    @IBOutlet weak var refreshButton: UIBarButtonItem!
+}
