ab7d29657c4f0688b053a1f2e3e3821a13ab3d04
Joshua Hunsberger <joshua@hunsberger.co>
2016-03-05 20:52:54 -0500

    Update references to JSON key struct

--- OnTheMap/ParseConvenience.swift
+++ OnTheMap/ParseConvenience.swift
@@ -25,7 +25,7 @@ extension ParseClient {
             if let error = error {
                 completionHandlerForGetLocations(success: false, error: error)
             } else {
-                guard let locationsList = results[JSONResponseKeys.results] as? [NSDictionary] else {
+                guard let locationsList = results[JSONKeys.results] as? [NSDictionary] else {
                     completionHandlerForGetLocations(success: false, error: NSError(domain: "getStudentLocations parsing", code: 1, userInfo: [NSLocalizedDescriptionKey: "Could not parse results list."]))
                     return
                 }
@@ -34,27 +34,27 @@ extension ParseClient {
                 
                 for location in locationsList {
                     // Extract values from JSON Student Location dictionary
-                    guard let firstName = location[JSONResponseKeys.firstName] as? String else {
+                    guard let firstName = location[JSONKeys.firstName] as? String else {
                         completionHandlerForGetLocations(success: false, error: NSError(domain: "getStudentLocations parsing", code: 1, userInfo: [NSLocalizedDescriptionKey: "Could not parse first name."]))
                         return
                     }
                     
-                    guard let lastName = location[JSONResponseKeys.lastname] as? String else {
+                    guard let lastName = location[JSONKeys.lastname] as? String else {
                         completionHandlerForGetLocations(success: false, error: NSError(domain: "getStudentLocations parsing", code: 1, userInfo: [NSLocalizedDescriptionKey: "Could not parse last name."]))
                         return
                     }
                     
-                    guard let latitude = location[JSONResponseKeys.latitude] as? Double else {
+                    guard let latitude = location[JSONKeys.latitude] as? Double else {
                         completionHandlerForGetLocations(success: false, error: NSError(domain: "getStudentLocations parsing", code: 1, userInfo: [NSLocalizedDescriptionKey: "Could not parse latitude."]))
                         return
                     }
                     
-                    guard let longitude = location[JSONResponseKeys.longitude] as? Double else {
+                    guard let longitude = location[JSONKeys.longitude] as? Double else {
                         completionHandlerForGetLocations(success: false, error: NSError(domain: "getStudentLocations parsing", code: 1, userInfo: [NSLocalizedDescriptionKey: "Could not parse longitude."]))
                         return
                     }
                     
-                    guard let url = location[JSONResponseKeys.urlString] as? String else {
+                    guard let url = location[JSONKeys.urlString] as? String else {
                         completionHandlerForGetLocations(success: false, error: NSError(domain: "getStudentLocations parsing", code: 1, userInfo: [NSLocalizedDescriptionKey: "Could not parse URL."]))
                         return
                     }
@@ -70,4 +70,14 @@ extension ParseClient {
     }
     
     //TODO: Create method to send student location with POST
+    func postStudentLocation(location: StudentLocation, completionHandlerForPostLocation: (success: Bool, error: NSError?) -> Void) {
+        
+        let jsonBody = "{\"\(JSONKeys.uniqueKey)\": \"\(location.uniqueKey)\", \"firstName\": \"\(location.firstName)\", \"lastName\": \"\(location.lastName)\",\"mapString\": \"\(location.mapString!)\", \"mediaURL\": \"\(location.mediaURL)\",\"latitude\": \(location.latitude), \"longitude\": \(location.longitude)}"
+
+        
+        taskForPostMethod(Methods.studentLocations, parameters: nil, jsonBody: jsonBody){ (results, error) in
+            
+            
+        }
+    }
 }
