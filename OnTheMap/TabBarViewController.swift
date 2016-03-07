08556b97ea3aca96868489da6372845568562176
Joshua Hunsberger <joshua@hunsberger.co>
2016-03-06 19:49:23 -0500

    Add action to logout button on tab bar controller Delete session from Udacity and return back to login view controller

--- OnTheMap/TabBarViewController.swift
+++ OnTheMap/TabBarViewController.swift
@@ -13,4 +13,16 @@ class TabBarViewController: UITabBarController {
     @IBOutlet weak var logoutButton: UIBarButtonItem!
     @IBOutlet weak var addLocationButton: UIBarButtonItem!
     @IBOutlet weak var refreshButton: UIBarButtonItem!
+    
+    @IBAction func logoutButtonPressed(sender: UIBarButtonItem) {
+        UdacityClient.sharedInstance().deleteSession() { (success, error) in
+            if(!success) {
+                print("Error while logging out. \(error!.localizedDescription)")
+            }
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+        }
+    }
 }
