ec5fd6e0ff063f937cfe9ecc8e0b934171a7d36f
Joshua Hunsberger <joshua@hunsberger.co>
2016-03-19 19:33:52 -0400

    Add getStudentLocations function to fetch and return new array of StudentLocations

--- OnTheMap/StudentLocationViewController.swift
+++ OnTheMap/StudentLocationViewController.swift
@@ -10,6 +10,42 @@ import UIKit
 
 class StudentLocationViewController: UIViewController {
     
+    func getStudentLocations() -> [StudentLocation]{
+        var studentLocations = [StudentLocation]()
+        
+        // Display activity view indicator
+        let activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0,0,50,50))
+        activityIndicator.activityIndicatorViewStyle = .Gray
+        view.addSubview(activityIndicator)
+        activityIndicator.center = view.center
+        activityIndicator.startAnimating()
+        
+        // Get new list of student locations from Parse
+        ParseClient.sharedInstance().getStudentLocations(100, skip: 0, order: "-updatedAt"){ (success, error) in
+            if(success){
+                guard let locations = ParseClient.sharedInstance().studentLocations else {
+                    dispatch_async(dispatch_get_main_queue()){
+                        activityIndicator.stopAnimating()
+                        //TODO: Display error
+                    }
+                    return
+                }
+                
+                dispatch_async(dispatch_get_main_queue()){
+                    activityIndicator.stopAnimating()
+                }
+                
+                studentLocations = locations
+            } else {
+                dispatch_async(dispatch_get_main_queue()){
+                    activityIndicator.stopAnimating()
+                    //TODO: Display errorß
+                }
+            }
+        }
+        return studentLocations
+    }
+    
     func refresh() {
         // Implement in subclass
     }
