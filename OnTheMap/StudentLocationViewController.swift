a2a301e6048ce9fec94024623d3231fb851ec131
Joshua Hunsberger <joshua@hunsberger.co>
2016-03-19 18:54:55 -0400

    Add StudentLocationViewController

---
+++ OnTheMap/StudentLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StudentLocationViewController.swift
+//  OnTheMap
+//
+//  Created by Joshua Hunsberger on 3/19/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class StudentLocationViewController: UIViewController {
+    
+}
