01613dc4f2bfd4e406d0026a4247d7605978aacc
Joshua Hunsberger <joshua@hunsberger.co>
2016-03-25 18:37:45 -0400

    Switch location label to text field and add IBOutlets to InformationPostingViewController

--- OnTheMap/InformationPostingViewController.swift
+++ OnTheMap/InformationPostingViewController.swift
@@ -7,8 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class InformationPostingViewController: UIViewController {
+    
+    //MARK: Interface Builder Outlets
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var linkTextField: UITextField!
+    @IBOutlet weak var whereLabel: UILabel!
+    @IBOutlet weak var studyingLabel: UILabel!
+    @IBOutlet weak var todayLabel: UILabel!
+    @IBOutlet weak var locationMapView: MKMapView!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var findButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
