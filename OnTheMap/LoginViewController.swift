2f5cef3f25f5d74dff9a215ed2d10e4ec2c98aec
Joshua Hunsberger <joshua@hunsberger.co>
2016-03-31 00:05:51 -0400

    Define attributes of text fields in view did load

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    let attributes = [
+        NSForegroundColorAttributeName : UIColor.whiteColor()
+    ]
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Use an invisible UI View to create padding for the left edge of the text fields.
+        // As described by StackOverflow user Evil Trout here: http://stackoverflow.com/a/4423805
+        
+        let emailPaddingView = UIView(frame: CGRectMake(0,0,10,30))
+        let passwordPaddingView = UIView(frame: CGRectMake(0,0,10,30))
+        emailTextField.leftView = emailPaddingView
+        emailTextField.leftViewMode = .Always
+        passwordTextField.leftView = passwordPaddingView
+        passwordTextField.leftViewMode = .Always
+        
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: attributes)
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: attributes)
     }
 
     @IBAction func loginButtonPressed(sender: AnyObject) {
